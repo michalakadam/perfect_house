b3ebdbe296614ec674e7a30527e53fdcfd31a2ec
Adam Michalak <adam.michalak.dev@gmail.com>
2020-12-03 20:59:50 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ConsultancyComponent } from './consultancy/consultancy.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { RentalManagementComponent } from './rental-management/rental-management.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'doradztwo',
+    component: ConsultancyComponent,
+  },
+  {
+    path: 'zarzadzanie',
+    component: RentalManagementComponent,
+  },
+  {
+    path: 'o-nas',
+    component: AboutComponent,
+  },
+  {
+    path: 'kontakt',
+    component: ContactComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
