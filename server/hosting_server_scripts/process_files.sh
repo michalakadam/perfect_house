c232e749d06d7539f67bd7d3193a2a953f672f0a
Adam Michalak <adam.michalak.dev@gmail.com>
2020-12-29 11:19:21 +0100

    refactor + improve logging

--- server/hosting_server_scripts/process_files.sh
+++ server/hosting_server_scripts/process_files.sh
@@ -7,6 +7,7 @@ offers_xml_path=$domain_folder_path/temp/oferty.xml
 
 set -- $agent_jpgs_path
 if [[ -f "$1" ]]; then
+  rm $domain_folder_path/public_html/agents/user_*.jpg
   cp $agent_jpgs_path $domain_folder_path/public_html/agents
 fi
 
@@ -15,19 +16,19 @@ if [[ -f "$1" ]]; then
   cp $offer_jpgs_path $domain_folder_path/public_html/offers
 fi
 
-if [[ "$(grep -q -m 1 "<Agenci />" $offers_xml_path)" -eq "0" ]]; then
+if [[ "$(grep --quiet --max-count=1 "<Agenci />" $offers_xml_path)" -eq "0" ]]; then
   touch $domain_folder_path/requiring_conversion/agents.xml
 else
   sed -n '/<Agenci/{n;:a;p;n;/<\/Agenci>/!ba;}' $offers_xml_path > $domain_folder_path/requiring_conversion/agents.xml
 fi
 
-if [[ "$(grep -q -m 1 "<Oferty />" $offers_xml_path)" -eq "0" ]]; then
+if [[ "$(grep --quiet --max-count=1 "<Oferty />" $offers_xml_path)" -eq "0" ]]; then
   touch $domain_folder_path/requiring_conversion/offers.xml
 else
   sed -n '/<Oferty/{n;:a;p;n;/<\/Oferty>/!ba;}' $offers_xml_path > $domain_folder_path/requiring_conversion/offers.xml
 fi
 
-if [[ "$(grep -q -m 1 "<Usuniete />" $offers_xml_path)" -eq "0" ]]; then
+if [[ "$(grep --quiet --max-count=1 "<Usuniete />" $offers_xml_path)" -eq "0" ]]; then
   touch $domain_folder_path/requiring_conversion/removed.xml
 else
   sed -n '/<Usuniete/{n;:a;p;n;/<\/Usuniete>/!ba;}' $offers_xml_path > $domain_folder_path/requiring_conversion/removed.xml
--- server/hosting_server_scripts/watch_ftp_folder.sh
+++ server/hosting_server_scripts/watch_ftp_folder.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
 domain_folder_path=/home/adam/Documents/oferty
-ftp_folder_path="$domain_folder_path"/public_ftp
+ftp_folder_path=$domain_folder_path/public_ftp
+
+# https://serverfault.com/a/103569
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+
+function start_logging {
+  log_file_name="$(date "+%Y_%m_%d_%H_%M")" 
+  log_file_path=$domain_folder_path/logs/$log_file_name
+  touch $log_file_path
+  exec 1>$log_file_path 2>&1
+}
 
 function check_file_copied_fully {
-  store_file_sizes_for_comparison $1 
+  store_file_sizes_for_comparison 
   while [[ "$old_size" -lt "$new_size" ]]
   do
-    store_file_sizes_for_comparison $1 
+    store_file_sizes_for_comparison 
   done
 }
 
 function store_file_sizes_for_comparison {
-  old_size=$(wc -c <$1)
+  old_size=$(wc --bytes < $offers_file_path)
   sleep 5
-  new_size=$(wc -c <$1)
+  new_size=$(wc --bytes < $offers_file_path)
 }
 
 while true
 do 
-  offers_file_name=$(ls "$ftp_folder_path")
-  if [[ "$offers_file_name"  ]]; then
-    log_file_path=$domain_folder_path/logs/"$(date "+%Y_%m_%d_%H_%M")"
-    touch $log_file_path
-    echo "New file arrived." > $log_file_path
-    check_file_copied_fully  $ftp_folder_path/$offers_file_name
-    echo "$(ls -l $ftp_folder_path)" >> $log_file_path
+  offers_file_name="$(ls $ftp_folder_path)"
+
+  if [[ ! -z "$offers_file_name" ]]; then
+    offers_file_path=$ftp_folder_path/$offers_file_name
+    
+    start_logging
+    check_file_copied_fully  
     unzip $ftp_folder_path/$offers_file_name -d $domain_folder_path/temp
     mv $ftp_folder_path/$offers_file_name $domain_folder_path/offers_archive/$offers_file_name
-    echo "File $offers_file_name unzipped to temp folder and moved to archives." >> "$log_file_path"
+
     $domain_folder_path/backend/process_files.sh
   fi
   sleep 10
