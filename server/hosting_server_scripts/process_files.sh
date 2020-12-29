c35c6744c29b8b19e39be7f8c4b744beab7cac84
Adam Michalak <adam.michalak.dev@gmail.com>
2020-12-29 08:03:33 +0100

    implement script splitting xml and copying jpgs

---
+++ server/hosting_server_scripts/process_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+domain_folder_path=/home/adam/Documents/oferty
+agent_jpgs_path=$domain_folder_path/temp/user_*.jpg
+offer_jpgs_path=$domain_folder_path/temp/ofe_*.jpg
+offers_xml_path=$domain_folder_path/temp/oferty.xml
+
+set -- $agent_jpgs_path
+if [[ -f "$1" ]]; then
+  cp $agent_jpgs_path $domain_folder_path/public_html/agents
+fi
+
+set -- $offer_jpgs_path
+if [[ -f "$1" ]]; then
+  cp $offer_jpgs_path $domain_folder_path/public_html/offers
+fi
+
+if [[ "$(grep -q -m 1 "<Agenci />" $offers_xml_path)" -eq "0" ]]; then
+  touch $domain_folder_path/requiring_conversion/agents.xml
+else
+  sed -n '/<Agenci/{n;:a;p;n;/<\/Agenci>/!ba;}' $offers_xml_path > $domain_folder_path/requiring_conversion/agents.xml
+fi
+
+if [[ "$(grep -q -m 1 "<Oferty />" $offers_xml_path)" -eq "0" ]]; then
+  touch $domain_folder_path/requiring_conversion/offers.xml
+else
+  sed -n '/<Oferty/{n;:a;p;n;/<\/Oferty>/!ba;}' $offers_xml_path > $domain_folder_path/requiring_conversion/offers.xml
+fi
+
+if [[ "$(grep -q -m 1 "<Usuniete />" $offers_xml_path)" -eq "0" ]]; then
+  touch $domain_folder_path/requiring_conversion/removed.xml
+else
+  sed -n '/<Usuniete/{n;:a;p;n;/<\/Usuniete>/!ba;}' $offers_xml_path > $domain_folder_path/requiring_conversion/removed.xml
+fi
+
--- server/hosting_server_scripts/watch_ftp_folder.sh
+++ server/hosting_server_scripts/watch_ftp_folder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 domain_folder_path=/home/adam/Documents/oferty
+ftp_folder_path="$domain_folder_path"/public_ftp
 
 function check_file_copied_fully {
   store_file_sizes_for_comparison $1 
@@ -18,15 +19,15 @@ function store_file_sizes_for_comparison {
 
 while true
 do 
-  offers_file_name=$(ls)
+  offers_file_name=$(ls "$ftp_folder_path")
   if [[ "$offers_file_name"  ]]; then
     log_file_path=$domain_folder_path/logs/"$(date "+%Y_%m_%d_%H_%M")"
-    touch log_file_path
+    touch $log_file_path
     echo "New file arrived." > $log_file_path
-    check_file_copied_fully  $offers_file_name
-    echo "$(ls -l $domain_folder_path/public_ftp)" >> $log_file_path
-    unzip $offers_file_name -d $domain_folder_path/temp
-    mv $offers_file_name $domain_folder_path/offers_archive/$offers_file_name
+    check_file_copied_fully  $ftp_folder_path/$offers_file_name
+    echo "$(ls -l $ftp_folder_path)" >> $log_file_path
+    unzip $ftp_folder_path/$offers_file_name -d $domain_folder_path/temp
+    mv $ftp_folder_path/$offers_file_name $domain_folder_path/offers_archive/$offers_file_name
     echo "File $offers_file_name unzipped to temp folder and moved to archives." >> "$log_file_path"
     $domain_folder_path/backend/process_files.sh
   fi
