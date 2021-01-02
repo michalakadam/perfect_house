bdfbc267a140131ff601e97b3809b728e0f97a4b
Adam Michalak <adam.michalak.dev@gmail.com>
2021-01-02 09:55:26 +0100

    split agents service into dao and converter

--- src/app/rental-management/rental-management.component.ts
+++ src/app/rental-management/rental-management.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { AgentsService } from '../services/agents.service';
+import { AgentsDao } from '../services/agents-dao.service';
 import { WindowSizeDetector } from '../services/window-size-detector.service';
 import { Agent } from '../shared/models/agent';4
 
@@ -17,13 +17,13 @@ export class RentalManagementComponent {
 
   constructor(
     readonly windowSizeDetector: WindowSizeDetector,
-    readonly agentsService: AgentsService,
+    readonly agentsDao: AgentsDao,
     private changeDetector: ChangeDetectorRef) {
       this.windowSizeDetector.windowSizeChanged$.subscribe(() => {
         this.changeDetector.detectChanges();
       });
 
       this.agentResponsibleForRentalManagement =
-        this.agentsService.getAgentByFullName(AGENT_RESPONSIBLE_FULL_NAME);
+        this.agentsDao.getAgentByFullName(AGENT_RESPONSIBLE_FULL_NAME);
   }
 }
