3d47851aa0478ad25bda0d66fc1e42d38b5f417a
Adam Michalak <adam.michalak.dev@gmail.com>
2021-01-04 06:49:34 +0100

    implement pagination

--- src/app/offers/offers.component.ts
+++ src/app/offers/offers.component.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { OffersDao } from '../services/offers-dao.service';
+import { Offer } from '../shared/models/offer';
 
 /** Strona wyświetla oferty nieruchomości oferując możliwość ich zaawansowanego wyszukiwania. */
 @Component({
@@ -9,10 +10,17 @@ import { OffersDao } from '../services/offers-dao.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OffersComponent {
+  offers: Offer[];
+  currentPage = 0;
+  isPaginatorVisible = true;
 
-  constructor(readonly offersDao: OffersDao) {}
+  constructor(readonly offersDao: OffersDao) {
+    this.offers = this.offersDao.list(this.currentPage);
+    this.isPaginatorVisible = this.offersDao.getNumberOfPages() !== 0;
+  }
 
-  paginate(event) {
-    // TODO: Implement pagination.
+  changePage(page: number) {
+    this.offers = this.offersDao.list(page);
+    this.currentPage = page;
   }
 }
--- src/app/services/offers-dao.service.ts
+++ src/app/services/offers-dao.service.ts
@@ -4,17 +4,40 @@ import * as rawOffers from "src/offers/offers.json";
 import { Offer } from '../shared/models/offer';
 import { OffersConverter } from './offers-converter.service';
 
+const OFFERS_PER_PAGE = 50;
+
 @Injectable({
     providedIn: 'root',
 })
 export class OffersDao {
-    private offers: Offer[];
+    private allOffers: Offer[];
+    private currentSearchOffers: Offer[];
 
     constructor(private offersConverter: OffersConverter) {
-        this.offers = this.offersConverter.convertToReadableOffers(rawOffers.Oferty.Oferta);
+        this.allOffers = this.offersConverter.convertToReadableOffers(rawOffers.Oferty.Oferta);
     }
 
-    listAll(): Offer[] {
-        return this.offers;
+    list(page: number): Offer[] {
+        // TODO: implement filtering and sorting.
+        this.currentSearchOffers = this.allOffers;
+
+        const startIndex = page * OFFERS_PER_PAGE; 
+        return this.currentSearchOffers.slice(startIndex, startIndex + OFFERS_PER_PAGE)
+    }
+
+    getOffersTotalQuantity(): number {
+        return this.currentSearchOffers.length;
+    }
+
+    getOffersPerPage(): number {
+        return OFFERS_PER_PAGE;
+    }
+
+    getNumberOfPages(): number {
+        const offersOverPageSize = this.currentSearchOffers.length / OFFERS_PER_PAGE; 
+        if (this.currentSearchOffers.length % OFFERS_PER_PAGE === 0) {
+            return offersOverPageSize - 1;
+        }
+        return Math.trunc(offersOverPageSize);
     }
 }
