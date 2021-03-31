8e43e7a77ed0b47eaabf46b411da6becafcf2fd2
Adam Michalak <adam.michalak.dev@gmail.com>
2021-03-31 07:39:41 +0200

    add newsfeed about shrooms picking

--- src/app/news-feed/news-feed.component.ts
+++ src/app/news-feed/news-feed.component.ts
@@ -5,6 +5,9 @@ const convertPhotoNameToUrl = (name: string): string => PHOTO_URL_PREFIX + name;
 
 const SEA_PHOTO_NAMES = ['morze_1.jpg', 'morze_2.jpg', 'morze_3.jpg', 'morze_4.jpg', 'morze_5.jpg', 'morze_6.jpg'];
 const BOWLING_PHOTO_NAMES = ['bowling_1.jpg', 'bowling_2.jpg', 'bowling_3.jpg', 'bowling_4.jpg'];
+const SHROOMS_PHOTO_NAMES = ['grzybobranie_1.jpg', 'grzybobranie_2.jpg', 'grzybobranie_3.jpg', 'grzybobranie_4.jpg',
+  'grzybobranie_5.jpg', 'grzybobranie_6.jpg', 'grzybobranie_7.jpg', 'grzybobranie_8.jpg', 'grzybobranie_9.jpg',
+   'grzybobranie_10.jpg', 'grzybobranie_11.jpg', ]
 
 @Component({
   selector: 'perfect-news-feed',
@@ -15,4 +18,5 @@ const BOWLING_PHOTO_NAMES = ['bowling_1.jpg', 'bowling_2.jpg', 'bowling_3.jpg', 
 export class NewsFeedComponent {
   seaPhotoUrls = SEA_PHOTO_NAMES.map(convertPhotoNameToUrl);
   bowlingPhotoUrls = BOWLING_PHOTO_NAMES.map(convertPhotoNameToUrl);
+  shroomsPhotoUrls = SHROOMS_PHOTO_NAMES.map(convertPhotoNameToUrl);
 }
