639bf884a43bb06b02fd51376f0bc59551378faf
Adam Michalak <adam.michalak.dev@gmail.com>
2021-10-02 14:04:55 +0200

    remove logging from website server scripts

--- server/website_server_scripts/process_files.sh
+++ server/website_server_scripts/process_files.sh
@@ -7,12 +7,6 @@ offer_jpgs_path=$temp_folder_path/*.jpg
 offers_xml_path=$temp_folder_path/oferty.xml
 server_credentials=root@51.77.195.170
 
-# https://serverfault.com/a/103569
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-log_file_path=$1
-exec 1>>$log_file_path 2>&1
-
 function copy_offer_jpgs {
   set -- $offer_jpgs_path
   if [[ -f "$1" ]]; then
@@ -56,4 +50,3 @@ rm $domain_folder_path/to_be_converted/*
 scp $temp_folder_path/to_be_converted.zip $server_credentials:/root/perfect/temp
 rm $temp_folder_path/to_be_converted.zip
 ssh $server_credentials '/root/perfect/convert_xml_to_json.sh'
-
