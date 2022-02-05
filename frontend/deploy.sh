a5618dd9a4d53376913bccd82a3b75d8616c1f1f
Adam Michalak <adam.michalak.dev@gmail.com>
2022-02-05 16:14:41 +0100

    update repo name

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,8 +5,8 @@ git pull origin master
 
 ng build
 # Remove test data so that does not override prod data.
-rm -r ~/Documents/perfecthouse/frontend/dist/perfecthouse/agents
-rm -r ~/Documents/perfecthouse/frontend/dist/perfecthouse/offers
+rm -r ~/Documents/perfect_house/frontend/dist/perfecthouse/agents
+rm -r ~/Documents/perfect_house/frontend/dist/perfecthouse/offers
 
-scp -r ~/Documents/perfecthouse/frontend/dist/perfecthouse/* perfect@s38.zenbox.pl:/home/perfect/domains/perfect.stronazen.pl/public_html
+scp -r ~/Documents/perfect_house/frontend/dist/perfecthouse/* perfect@s38.zenbox.pl:/home/perfect/domains/perfect.stronazen.pl/public_html
 
