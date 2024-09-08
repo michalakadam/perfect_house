734c21fa341bd15877002b27e639fc194f3b56e3
Adam Michalak <adam.michalak.dev@gmail.com>
2024-09-08 14:14:19 +0200

    chore: streamline communication across servers

--- server/website_server_scripts/process_files.sh
+++ server/website_server_scripts/process_files.sh
@@ -5,6 +5,10 @@ temp_folder_path=$domain_folder_path/temp
 # There are many patterns for offers jpg file names.
 offer_jpgs_path=$temp_folder_path/*.jpg
 offers_xml_path=$temp_folder_path/oferty.xml
+extracted_offers_xml_path=$domain_folder_path/to_be_converted/offers.xml
+extracted_removed_offers_xml_path=$domain_folder_path/to_be_converted/removed.xml
+db_server_credentials=root@51.77.195.170
+db_server_xml_folder_path=/root/perfect/temp
 
 function copy_offer_jpgs {
   set -- $offer_jpgs_path
@@ -20,9 +24,9 @@ function extract_offers_xml {
       p
       n
       /<\/Oferty>/!ba
-      }' $offers_xml_path > $domain_folder_path/to_be_converted/offers.xml
-    sed -i '1s/^/<Oferty>\n/' $domain_folder_path/to_be_converted/offers.xml
-    echo "</Oferty>" >> $domain_folder_path/to_be_converted/offers.xml 
+      }' $offers_xml_path > $extracted_offers_xml_path
+    sed -i '1s/^/<Oferty>\n/' $extracted_offers_xml_path
+    echo "</Oferty>" >> $extracted_offers_xml_path
   fi
 }
 
@@ -33,13 +37,22 @@ function extract_removed_xml {
       p
       n
       /<\/Usuniete>/!ba
-      }' $offers_xml_path > $domain_folder_path/to_be_converted/removed.xml
-    sed -i '1s/^/<Usuniete>\n/' $domain_folder_path/to_be_converted/removed.xml
-    echo "</Usuniete>" >> $domain_folder_path/to_be_converted/removed.xml 
+      }' $offers_xml_path > $extracted_removed_offers_xml_path
+    sed -i '1s/^/<Usuniete>\n/' $extracted_removed_offers_xml_path
+    echo "</Usuniete>" >> $extracted_removed_offers_xml_path 
   fi
+}
+
+function trigger_db_server {
+  scp $extracted_offers_xml_path $db_server_credentials:$db_server_xml_folder_path
+  rm $extracted_offers_xml_path
+  scp $extracted_removed_offers_xml_path $db_server_credentials:$db_server_xml_folder_path
+  rm $extracted_removed_offers_xml_path
+  ssh $db_server_credentials "/root/perfect/convert_xml_to_json.sh"
 }
 
 copy_offer_jpgs
 extract_offers_xml
 extract_removed_xml
 rm $offers_xml_path
+trigger_db_server
